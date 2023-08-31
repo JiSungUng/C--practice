20c214683e1d59ebd29cf9db3b3b3b54d1e94725
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-08-31 15:51:23 +0900

    연산자 + 배열

---
+++ chapter2/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter2
+{
+    internal class Array
+    {
+
+        //1차원배열
+        /*  int[] a = { 1, 2, 3, 4 };
+          string[] vars = { "사과", "바나나", "사과" };
+
+          Console.WriteLine(vars[1]);
+
+          //2차원배열
+        int [,] abc={{1,2,3},{4,5,9},{6,7,8}}; // 열크기가 동일해야함.
+
+        //가변 배열
+        int[][] ab = new int [3][];
+        ab[0]=new int [4]; // 첫번째 열의 크기는 4
+        ab[1]=new int [3]; // 두번째 열의 크기는 3
+        ab[2]=new int [2]; // 세번째 열의 크기는 2
+
+}*/
+    }
+}
+
---
+++ chapter2/chapter3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*[단항연산자]
+피연산자를 a, b, c의 변수로가정.
+EX)+a,-a,++a,--a,!a
+연산에 필요한 항이 한개.
+
+[이항연산자]
+피연산자를 a, b, c의 변수로가정.
+EX)a = b – c, a -= b, a += b, a *= b
+b-c계산 후 a=(b-c결과)대입.
+연산에 필요한 항이 두개
+
+[삼항연산자]
+피연산자를 a, b, c의 변수로가정.
+EX)(조건식) ? A : B
+조건식에 따라 A또는 B중 하나의결과 반환. 참이면 A , 거짓일 때는 B 
+*/
+
+namespace chapter2
+{
+    internal class Chapter3
+    {
+    }
+}
